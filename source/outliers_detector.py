91f56b8651c7fa8da392d3cdec7b0a9dd8d5884d
Eyal Cohen (Networking) <eyal.cohen01@audiocodes.com>
2022-03-04 16:02:19 +0200

    Initial commit

---
+++ source/outliers_detector.py
@@ -0,0 +1,150 @@
+import os
+import sys
+import warnings
+
+import numpy as np
+import pandas as pd
+from pyod.models.cof import COF
+from pyod.models.ocsvm import OCSVM
+from pyod.models.knn import KNN
+from pyod.models.mcd import MCD
+from pyod.models.pca import PCA as PCA_pyod
+from pyod.models.loda import LODA
+from pyod.models.copod import COPOD
+from pyod.models.ecod import ECOD
+from pyod.models.so_gaal import SO_GAAL
+
+from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+warnings.filterwarnings("ignore")
+
+stdout = sys.stdout
+
+# Disable
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+
+# Restore
+def enablePrint():
+    sys.stdout = stdout
+
+
+class OutlierGridSearcher(object):
+    def __init__(self, train_func, higher_is_better=True, scoring_weights=None, algs_to_pass=None, verbose=False):
+        """
+        Init function
+
+        :param train_func: The training function, should be a function that gets X,y and returns array of scores that
+                will be optimized, the calculated mean of the scores would be our score to optimized.
+
+        :param higher_is_better: If we want to find the max or the min of the scores.
+
+        :param scoring_weights: Weights of the scores, if one score from the returned list from the training func is
+                more important we can give weights to be used when calculating the mean.
+
+        :param algs_to_pass: List og algorithms we don't want to test.
+
+        :param verbose: If to print the outliers we dropped.
+
+        """
+        self._train_func = train_func
+        self._higher_is_better = higher_is_better
+        self._scoring_weights = scoring_weights
+        self._algs = [COF(), OCSVM(), KNN(), MCD(), PCA_pyod(), LODA(), COPOD(), ECOD(), SO_GAAL()]
+        self._algs_names = ['COF', 'OCSVM', 'KNN', 'MCD', 'PCA', 'LODA', 'COPOD', 'ECOD', 'SO_GAAL']
+        self._verbose = verbose
+        if algs_to_pass is not None:
+            # deleting the algorithms we don't want to test
+            del self._algs[algs_to_pass]
+            del self._algs_names[algs_to_pass]
+
+    def _get_outliers_scores(self, alg, top_num, X):
+        # Creating a score column
+        X["score"] = (alg.decision_scores_ / sum(alg.decision_scores_)).tolist()
+        drop_rows = X.sort_values("score", ascending=False).head(top_num)
+        X.drop(["score"], axis=1)
+        # returning a list of indexes to drop from the df
+        return drop_rows.index.values.tolist()
+
+    def _remove_outliers(self, Outlier_indexes, X, y=None, verbose=False, visualize=False):
+
+        pca = PCA(n_components=2)
+        principalComponents = pca.fit_transform(X)
+        if verbose:
+            # prints the lines we remove from the df
+            print("droping the following rows:")
+            print(X.iloc[Outlier_indexes])
+        principalDf = pd.DataFrame(data=principalComponents
+                                   , columns=['principal component 1', 'principal component 2'])
+        if visualize:
+            plt.title("Data visualization")
+            plt.scatter(principalDf['principal component 1'], principalDf['principal component 2'], label='sample')
+            red_points = principalDf.iloc[Outlier_indexes]
+            plt.scatter(red_points['principal component 1'], red_points['principal component 2'],
+                        c='red', label='outlier')
+            plt.legend()
+            # shows a PCA plot of the indexes we remove
+            plt.show()
+
+        if y is not None:
+            y_dropped = y.drop(labels=Outlier_indexes, axis=0)
+        else:
+            y_dropped = y
+        X_dropped = X.drop(labels=Outlier_indexes, axis=0)
+        return X_dropped, y_dropped
+
+    def fit(self, X, y=None, top_nums_to_remove=[0, 5, 10, 20, 30]):
+        scores = []
+        for i, _alg in enumerate(self._algs):
+            enablePrint()
+            print("testing " + self._algs_names[i])
+            blockPrint()
+
+            if y is not None:
+                _alg.fit(X, y)
+            else:
+                _alg.fit(X)
+
+            scores_nums = {}
+
+            for top_num in top_nums_to_remove:
+                # getting outliers scores
+                Outlier_indexes = self._get_outliers_scores(_alg, top_num, X)
+
+                # removing suspected outliers
+                X_dropped, y_dropped = self._remove_outliers(Outlier_indexes, X, y)
+
+                # running the training func and getting the scores
+                score_nums = self._train_func(X_dropped, y_dropped)
+
+                # appending mean to the scores
+                if self._scoring_weights is not None:
+                    scores_nums[top_num] = np.average(score_nums, weights=self._scoring_weights)
+                else:
+                    scores_nums[top_num] = np.average(score_nums)
+
+            scores.append(scores_nums)
+
+        models_score = pd.DataFrame(index=self._algs_names, data=scores)
+        if self._higher_is_better:
+            best_score = models_score.stack().index[np.argmax(models_score.values)]
+        else:
+            best_score = models_score.stack().index[np.argmin(models_score.values)]
+
+        enablePrint()
+        print("\n\n### OUTPUT ###\nThe best score was achieved using " + best_score[0] + " when filtering " + str(
+            best_score[1]) + " outliers")
+        print(models_score)
+        # getting outliers scores
+        Outlier_indexes = self._get_outliers_scores(self._algs[self._algs_names.index(best_score[0])], best_score[1], X)
+
+        # removing suspected outliers
+        X_dropped, y_dropped = self._remove_outliers(Outlier_indexes, X, y, verbose=self._verbose, visualize=True)
+
+        if y_dropped is not None:
+            return X_dropped, y_dropped
+        else:
+            return X_dropped
---
+++ source/outliers_detector_utils.py
@@ -0,0 +1,123 @@
+import sys
+import warnings
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import StratifiedKFold, train_test_split, cross_validate
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import linear_model
+
+warnings.filterwarnings("ignore")
+
+
+def load_data(target, path):
+    df = pd.read_csv(path)
+    str_col = [col for col in df.columns if df[col].dtype == 'object']
+    df_nontree = pd.get_dummies(df, columns=str_col, drop_first=False)
+
+    X, y = df_nontree.drop(target, axis=1), df_nontree[target]
+
+    X = X.apply(lambda x: x.fillna(x.mean()), axis=0)
+
+    return X, y
+
+
+def load_data_heartFailurePrediction():
+    return load_data('target', 'Datasets/heartFailurePrediction.csv')
+
+
+def load_data_HeartDiseaseUCI():
+    return load_data('HeartDisease', 'Datasets/HeartDiseaseUCI.csv')
+
+
+def train_model_heart(X, y):
+    kf = StratifiedKFold(n_splits=5)
+    log = make_pipeline(StandardScaler(), LogisticRegression())
+    log_score = cross_validate(log, X=X, y=y, cv=kf, scoring=['accuracy', 'recall', 'precision', 'f1'])
+    log_score = pd.DataFrame(log_score).mean()
+    log_score = log_score[2:]
+    return log_score.tolist()
+
+
+def load_data_house():
+    X, y = load_data('SalePrice', 'Datasets/HousePrices/train.csv')
+
+    # ===========================================================================
+    # select some features
+    # ===========================================================================
+    features = ['LotArea', 'OverallQual', 'OverallCond', 'YearBuilt',
+                'YearRemodAdd', 'BsmtFinSF1', 'BsmtFinSF2', 'TotalBsmtSF',
+                '1stFlrSF', 'LowQualFinSF', 'GrLivArea', 'BsmtFullBath',
+                'BsmtHalfBath', 'HalfBath', 'BedroomAbvGr', 'Fireplaces',
+                'GarageCars', 'GarageArea', 'WoodDeckSF', 'OpenPorchSF',
+                'EnclosedPorch', 'PoolArea', 'YrSold']
+
+    X = X[features]
+    return X, y
+
+
+def nutrelize_inf(num):
+    if np.isinf(num):
+        return sys.float_info.max
+    else:
+        return num
+
+
+def train_model_house(X, y):
+    y = np.log1p(y)
+
+    X = X.to_numpy()
+    y = y.to_numpy().reshape(-1, 1)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    y_train, y_test = y_train.reshape(-1, 1), y_test.reshape(-1, 1)
+
+    regr = linear_model.LinearRegression()
+    regr.fit(X_train, y_train)
+
+    y_pred = regr.predict(X_test)
+
+    rms = mean_squared_error(y_test, y_pred)
+
+    print("The rms score is %.5f" % rms)
+    return [rms]
+
+
+def load_data_Cardiotocography():
+    data = pd.read_excel('Datasets/Cardiotocography.xls', sheet_name=1, skiprows=1)
+    data.drop(data.iloc[:, :10], inplace=True, axis=1)
+    data.drop(data.iloc[:, 22:33], inplace=True, axis=1)
+    data = data.drop(['Unnamed: 31', 'Unnamed: 44'], axis=1)
+    data.isna().sum()
+    data = data.dropna()
+    data = data.drop_duplicates()
+    X = data.drop(['CLASS'], axis=1)
+    y = data['CLASS']
+    return X, y
+
+
+def train_model_Cardiotocography(X, y):
+    # Train test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+    # We will produce synthetic data to balance the classes
+    sm = SMOTE(random_state=42)
+    X_train, y_train = sm.fit_resample(X_train, y_train)
+
+    # We will apply Standard Scaling to our data
+    scaler = StandardScaler()
+    standardized_X_train = pd.DataFrame(scaler.fit_transform(X_train), columns=X_train.columns)
+    standardized_X_test = pd.DataFrame(scaler.transform(X_test), columns=X_test.columns)
+
+    clf_gini = DecisionTreeClassifier(criterion='gini', max_depth=6, random_state=42)
+    clf_gini.fit(standardized_X_train, y_train)
+
+    y_pred_gini = clf_gini.predict(standardized_X_test)
+
+    return [accuracy_score(y_test, y_pred_gini)]
